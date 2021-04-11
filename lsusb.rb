300f76c0c7e18e09e3b64ff3daac3c0f31c35bef
Ralph Martin <ralph@cs.cf.ac.uk>
2021-04-11 07:49:16 +0100

    Update lsusb.rb
    Change file to match current style recommendation. No significant change to content. :-)

--- lsusb.rb
+++ lsusb.rb
@@ -1,15 +1,12 @@
-require 'formula'
-
 class Lsusb < Formula
-  homepage 'https://github.com/jlhonora/lsusb'
-  head 'https://github.com/jlhonora/lsusb', :using => :git
-
-  url 'https://github.com/jlhonora/lsusb/releases/download/1.0/lsusb-1.0.tar.gz'
-  sha256 '68cfa4a820360ecf3bbd2a24a58f287d41f66c62ada99468c36d5bf33f9a3b94'
+  desc "A utility for displaying information about USB buses in the system and devices connected to them"
+  homepage "https://github.com/jlhonora/lsusb"
+  url "https://github.com/jlhonora/lsusb/releases/download/1.0/lsusb-1.0.tar.gz"
+  sha256 "68cfa4a820360ecf3bbd2a24a58f287d41f66c62ada99468c36d5bf33f9a3b94"
+  head "https://github.com/jlhonora/lsusb", using: :git
 
   def install
-    bin.install 'lsusb'
-    man8.install 'man/lsusb.8'
+    bin.install "lsusb"
+    man8.install "man/lsusb.8"
   end
-
 end
